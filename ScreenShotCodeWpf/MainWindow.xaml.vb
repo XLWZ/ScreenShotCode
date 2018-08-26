91c566d98e62955f1d5967aece98cb7e4b74aea5
曾文誉 <xlwzforever@outlook.com>
2018-08-26 12:10:10 +0800

    Update to WPF

---
+++ ScreenShotCodeWpf/MainWindow.xaml.vb
@@ -0,0 +1,84 @@
+﻿Class MainWindow
+    Private Sub MainWindow_Initialized(sender As Object, e As EventArgs) Handles Me.Initialized
+
+    End Sub
+
+    Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        TxtBoxDate.Text = DateTime.Now.ToString("yyyy/MM/dd")
+    End Sub
+
+    Private Sub LstBoxInput_DragEnter(sender As Object, e As DragEventArgs) Handles LstBoxInput.DragEnter
+        e.Effects = IIf(e.Data.GetDataPresent(DataFormats.FileDrop), DragDropEffects.Link, DragDropEffects.None)
+    End Sub
+
+    Private Sub LstBoxInput_Drop(sender As Object, e As DragEventArgs) Handles LstBoxInput.Drop
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            Dim InputFiles() As String
+
+            InputFiles = e.Data.GetData(DataFormats.FileDrop)
+
+            For Each eachFile As String In InputFiles
+                If (IO.Path.GetExtension(eachFile).ToLower = ".png") Then
+                    If Not LstBoxInput.Items.Contains(System.Text.RegularExpressions.Regex.Replace(IO.Path.GetFileNameWithoutExtension(eachFile), "[^0-9]", "")) Then
+                        LstBoxInput.Items.Add(System.Text.RegularExpressions.Regex.Replace(IO.Path.GetFileNameWithoutExtension(eachFile), "[^0-9]", ""))
+                    End If
+                End If
+            Next
+
+        End If
+    End Sub
+
+    Private Sub LstBoxInput_KeyUp(sender As Object, e As KeyEventArgs) Handles LstBoxInput.KeyUp
+        If (e.Key = Key.Delete) Then
+            LstBoxInput.Items.Remove(LstBoxInput.SelectedItem)
+        End If
+    End Sub
+
+    Private Sub BtnGet_Click(sender As Object, e As RoutedEventArgs) Handles BtnGet.Click
+        TxtBoxOutput.Clear()
+
+        Dim StrInputFrames() As String = LstBoxInput.Items.Cast(Of String)().ToArray()
+
+        Dim TempArr(StrInputFrames.GetUpperBound(0)) As Integer
+
+        For index = 0 To TempArr.GetUpperBound(0)
+            TempArr(index) = Convert.ToInt32(StrInputFrames(index))
+        Next
+        Array.Sort(TempArr)
+        For index = 0 To StrInputFrames.GetUpperBound(0)
+            StrInputFrames(index) = Convert.ToString(TempArr(index))
+        Next
+
+        For index = 0 To StrInputFrames.GetUpperBound(0)
+            StrInputFrames(index) = TxtBoxDate.Text & "/" & StrInputFrames(index)
+        Next
+
+        HTML(StrInputFrames)
+        BBCode(StrInputFrames)
+        MarkDown(StrInputFrames)
+    End Sub
+
+    Private Sub HTML(ByVal InputFrames() As String)
+        TxtBoxOutput.AppendText("Comparison (right click on the image and open it in a new tab to see the full-size one)<br/>" & vbCrLf & "Source________________________________________________Encode<br/>" & vbCrLf)
+        For Each eachFrame In InputFrames
+            TxtBoxOutput.AppendText("<a href=""http://img.2222.moe/images/" & eachFrame & ".png""><img src=""http://img.2222.moe/images/" & eachFrame & "s.png""></a> <a href=""http://img.2222.moe/images/" & eachFrame & "v.png""><img src=""http://img.2222.moe/images/" & eachFrame & "s.png""></a><br/>" & vbCrLf)
+        Next
+        TxtBoxOutput.AppendText(vbCrLf & vbCrLf)
+    End Sub
+
+    Private Sub BBCode(ByVal InputFrames() As String)
+        TxtBoxOutput.AppendText("Comparison (right click on the image and open it in a new tab to see the full-size one)" & vbCrLf & "Source________________________________________________Encode" & vbCrLf)
+        For Each eachFrame In InputFrames
+            TxtBoxOutput.AppendText("[URL=http://img.2222.moe/images/" & eachFrame & ".png][IMG]http://img.2222.moe/images/" & eachFrame & "s.png[/IMG][/URL] [URL=http://img.2222.moe/images/" & eachFrame & "v.png][IMG]http://img.2222.moe/images/" & eachFrame & "s.png[/IMG][/URL]" & vbCrLf)
+        Next
+        TxtBoxOutput.AppendText(vbCrLf & vbCrLf)
+    End Sub
+
+    Private Sub MarkDown(ByVal InputFrames() As String)
+        TxtBoxOutput.AppendText("Comparison (right click on the image and open it in a new tab to see the full-size one)" & vbCrLf & "Source________________________________________________Encode" & vbCrLf)
+        For Each eachFrame In InputFrames
+            TxtBoxOutput.AppendText("[![](http://img.2222.moe/images/" & eachFrame & "s.png)](http://img.2222.moe/images/" & eachFrame & ".png) [![](http://img.2222.moe/images/" & eachFrame & "s.png)](http://img.2222.moe/images/" & eachFrame & "v.png)" & vbCrLf)
+        Next
+    End Sub
+
+End Class
